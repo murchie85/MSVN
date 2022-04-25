96608e430e943283120d7d55b619353f639b90ba
murchie85 <murchie85@gmail.com>
2022-04-25 10:04:13 +0100

    small

---
+++ src/main.py
@@ -0,0 +1,11 @@
+"""
+Client 
+
+1. Select Media
+2. Generate request token 
+3. Send authentication header in json
+4. Save transaction (but can be lost, file also can be lost)
+5. Accept reciept: if media,hist not there then cached for later
+
+
+"""
